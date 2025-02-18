7e4955238d6ba912a354adbca09145b3ceb81803
Gabriel Gatimu <geraldg652@gmail.com>
2025-02-18 08:15:18 +0300

    update routing to book loans

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :books do
+    member do
+      post :borrow
+      patch :return
+    end
+  end
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # get "/borrowers/:name", to: "loans#borrower_history", as: "borrower_history"
+  get "/borrower_history", to: "loans#borrower_history", as: "borrower_history"
+  resources :loans, only: [ :index ]
+
+  root "books#index"
+
+  # Health check route
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
