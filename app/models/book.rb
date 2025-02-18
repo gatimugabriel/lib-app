1f0051ce1c5f74a9b56a073dbb6d7035fe239879
Gabriel Gatimu <geraldg652@gmail.com>
2025-02-18 08:02:24 +0300

    update: book model -> check if book is borrowed

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ApplicationRecord
   has_many :loans, dependent: :destroy
   validates :title, :author, presence: true
+
+  def currently_borrowed?
+    loans.exists?(returned_on: nil)
+  end
 end
