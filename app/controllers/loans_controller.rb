43de887b9070ef11b398b019fb4ee35597404614
Gabriel Gatimu <geraldg652@gmail.com>
2025-02-19 21:47:55 +0300

    fix: return book bug -> return button now redirects to return page where they can see all details about the book loan

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -5,15 +5,10 @@ class LoansController < ApplicationController
     @loans = Loan.all.order(borrowed_on: :desc)
   end
 
-  # def borrower_history
-  #   @borrower_name = params[:borrower_name]
-  #   @loans = Loan.where(borrower_name: @borrower_name).order(borrowed_on: :desc)
-  # end
-
   def borrower_history
     @borrower_name = params[:borrower_name]
     @loans = Loan.where(borrower_name: @borrower_name).order(borrowed_on: :desc)
-    
+
     if @loans.empty?
       flash[:alert] = "No loans found for borrower '#{@borrower_name}'"
       redirect_to loans_path
